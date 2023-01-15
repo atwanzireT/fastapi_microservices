e441c73f36a87408adf26281ad0962b8271f162b
atwanzireT <tatwanzire@gmail.com>
2023-01-15 19:24:17 +0300

    order server requesting information from th inventory server

--- payment/main.py
+++ payment/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
 from starlette.requests import Request
 import requests
+from time import time
 
 app = FastAPI()
 
@@ -22,18 +23,37 @@ redis = get_redis_connection(
 )
 
 class Order(HashModel):
-    product_id : str
-    price : str
-    fee : float
-    total : float
-    quantity : int
-    status : str
+    product_id: str
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str  # pending, completed, refunded
 
     class Meta:
         database = redis
 
 @app.post('/orders')
-async def create_order(request : Request):
+async def create_order(request : Request): # id Quantity
     body = await request.json()
-    req = requests.get("http://localhost/products/%s" % body['id'])
-    return request.json()
+    req = requests.get('http://localhost:8000/products/%s' % body['id'])
+    product = req.json()
+
+    
+    order = Order(
+        product_id=body['id'],
+        price=product['price'],
+        fee=0.2 * product['price'],
+        total=1.2 * product['price'],
+        quantity=body['quantity'],
+        status='pending'
+    )
+    order.save()
+    order_completed(order)
+    return order
+
+def order_completed(order: Order):
+    time.sleep(5)
+    order.status = 'completed'
+    order.save()
+    # redis.xadd('order_completed', order.dict(), '*')
